52341f04d447252d978187ab3ef93e95cf85c2ba
varshith-s <103318727+varshith-s@users.noreply.github.com>
2022-08-19 17:51:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function colortoaqua()
+{
+    document.getElementById("color").style.backgroundColor="aqua";
+    document.getElementById("change").innerHTML="Hello! Iam Aqua 😊";
+}
+function colortored()
+{
+    document.getElementById("color").style.backgroundColor="red";
+    document.getElementById("change").innerHTML="Yep! I changed to Red now 🥰";
+}
+function colortoviolet()
+{
+    document.getElementById("color").style.backgroundColor="violet";
+    document.getElementById("change").innerHTML="Hi, My name is Violet 😍";
+}
+function colortobv()
+{
+    document.getElementById("color").style.backgroundColor="blueviolet";
+    document.getElementById("change").innerHTML="Thank you for clicking on BlueViolet 😉";
+}
+function colortocrim()
+{
+    document.getElementById("color").style.backgroundColor="crimson";
+    document.getElementById("change").innerHTML="on...son....I am Crimson 😁";
+}
+function colortodg()
+{
+    document.getElementById("color").style.backgroundColor="darkgray";
+    document.getElementById("change").innerHTML="Hey There! Here The DarkGray 😎";
+}
+function colortosg()
+{
+    document.getElementById("color").style.backgroundColor="springgreen";
+    document.getElementById("change").innerHTML="Do you like spring? This is SpringGreen 😘";
+}
+
+  
